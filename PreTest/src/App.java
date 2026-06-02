d1bed24ca1a1f05d8a1b676b5e57fef5339eefb8
Axcelion017 <michaelnewton1708@gmail.com>
2026-06-02 14:13:02 +0700

    PreTest

--- PreTest/src/App.java
+++ PreTest/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        int num1, num2, hasil;
+
+        try{
+            System.out.print("Masukan angka pertama: ");
+            num1  = input.nextInt();
+            System.out.print("Masukan angka kedua: ");
+            num2 = input.nextInt();
+            hasil = num1 / num2;
+        }catch (ArithmeticException e) {
+            System.out.println("Error: Pembagian dengan nol tidak diperbolehkan. Error: " + e.getMessage());
+        }catch(InputMismatchException i){
+            System.out.println("Error: Input harus berupa angka. Error: " + i.getMessage());
+        }finally{
+            System.out.println("Program kelar nih bos.");
+        }
+        input.close();
     }
 }
+
+
